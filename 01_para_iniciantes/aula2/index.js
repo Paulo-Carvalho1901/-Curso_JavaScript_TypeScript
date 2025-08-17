a0d00ad8f6e49a7e789ded3a8f150e7b413206dc
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-17 02:17:59 -0300

    comentarios em js

---
+++ 01_para_iniciantes/aula2/index.js
@@ -0,0 +1,9 @@
+// E escrever um comentario - comantario em linha
+console.log('Olá, mundo'); // Aqui outro comentario
+console.log('Linha 1');
+// comentario nesse local
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
+
