7be6717d8091c3d858b58960d1e4ea61135eea6e
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-18 11:31:54 -0300

    declarando uma variavel com let e inicializando com valor

--- 01_para_iniciantes/aula5/index.js
+++ 01_para_iniciantes/aula5/index.js
@@ -1,7 +1,5 @@
-let nome = "João"; // criando uma variavel nome
-
-console.log(nome, "nasceu em 1994");
-console.log("Em 2000", nome, "conheceu Maria,");
-console.log(nome, "casou-se com Maria em  2012,");
-console.log("Maria teve 1 filho com", nome, "em 2015.");
-console.log("O filho de", nome, "se chama Eduardo.");
+let nome;
+nome = "Qualquer valor"; // iniciando a variavel declarada
+console.log(nome);
+nome = "Paulo Carvalho";
+console.log(nome);
