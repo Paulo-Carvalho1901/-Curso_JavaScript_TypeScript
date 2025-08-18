05b86e671ccc179e744f3ff9ab9175f731b9ac20
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-18 11:26:50 -0300

    declarando a primeira variavel em js

---
+++ 01_para_iniciantes/aula5/index.js
@@ -0,0 +1,7 @@
+let nome = "João"; // criando uma variavel nome
+
+console.log(nome, "nasceu em 1994");
+console.log("Em 2000", nome, "conheceu Maria,");
+console.log(nome, "casou-se com Maria em  2012,");
+console.log("Maria teve 1 filho com", nome, "em 2015.");
+console.log("O filho de", nome, "se chama Eduardo.");
