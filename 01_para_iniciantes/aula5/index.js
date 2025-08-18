d080e22dfc091135718a37707ef8cf3de8d7ef63
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-18 11:47:36 -0300

    Regras para criar variaveis em js

--- 01_para_iniciantes/aula5/index.js
+++ 01_para_iniciantes/aula5/index.js
@@ -3,3 +3,42 @@ nome = "Qualquer valor"; // iniciando a variavel declarada
 console.log(nome);
 nome = "Paulo Carvalho";
 console.log(nome);
+
+/*
+Regras para criação de variaveis
+Não podemos criar variaveis com palavras reservadas.
+
+let if;
+let console;
+let var;
+
+Variaveis precisa ter nomes significativos
+
+let n = 'João'; o que seria n ???
+
+Não pode começa o nome de uma variavel com número
+
+let 1nome;
+
+Não pode conter espaços ou traços
+
+let primeiro nome;
+let contas a pagar;
+
+Utilizamos para nomes de variavel com mais de uma palavra camelcase
+
+let nomeCompleto;
+
+Case-sensitive
+
+let nomeCliente = 'Paulo';
+let nomecliente = 'Carvalho';
+
+Não podemos redeclarar variavel com LET
+
+let cliente = 'Paulo';
+cliente = 'Andreia';
+let cliente = 'Otavio';
+
+Para criar variavel não utilize VAR utilize LET
+*/
