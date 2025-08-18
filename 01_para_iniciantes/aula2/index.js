05d8f612364e9934cb2cf072dad4f8c65be69a71
Paulo Carvalho <paulo.carvalho@global.ntt>
2025-08-18 10:39:24 -0300

    comentario de bloco

--- 01_para_iniciantes/aula2/index.js
+++ 01_para_iniciantes/aula2/index.js
@@ -1,9 +1,14 @@
 // E escrever um comentario - comantario em linha
-console.log('Olá, mundo'); // Aqui outro comentario
-console.log('Linha 1');
+console.log("Olá, mundo"); // Aqui outro comentario
+console.log("Linha 1");
 // comentario nesse local
-console.log('Linha 2');
-console.log('Linha 3');
-console.log('Linha 4');
-// Vai ser ignorado
 
+/*
+Comentario de bloco
+para documentação
+ou explicação de código
+*/
+console.log("Linha 2");
+console.log("Linha 3");
+console.log("Linha 4");
+// Vai ser ignorado
