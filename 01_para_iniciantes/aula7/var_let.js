67e38947c6e497782857a48cc640e2e8027dd920
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-03-05 16:43:21 -0300

    diferenças entre var e let

---
+++ 01_para_iniciantes/aula7/var_let.js
@@ -0,0 +1,9 @@
+// ECMAScript 2015 (ES6) padroniza~ção da linguagem
+// var nome = 'Paulo Carvalho';
+// var nome = 'Carvalho';
+
+// nome = 'Carvalho' // NÃO UTILIZE ISSO
+
+let nome = 'Paulo';
+
+console.log(nome)
