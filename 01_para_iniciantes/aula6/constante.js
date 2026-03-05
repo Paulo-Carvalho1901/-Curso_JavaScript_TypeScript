f5281a763d17ea7659f464fa227b545b40c977f3
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-03-05 15:14:15 -0300

    (conts) introdução as constantes em js

---
+++ 01_para_iniciantes/aula6/constante.js
@@ -0,0 +1,17 @@
+// NÃO PODEMOS CRIAR CONSTANTES COM PALAVRAS RESERVADAS
+// CONSTANTES PRECISAM TER NOMES SIGNIFICATIVOS
+// NÃO PODE COMEÇAR O NOME DE UMA CONSTANTE COM UM NÚMERO
+// NÃO PODEM CONTER ESPAÇOS OU TRAÇOS
+// UTILIZAMOS COMELCASE
+// CASE-SENSITVE
+// NÃO PODE MODIFICAR O VALOR DE UMA CONSTANTE
+// NÃO UTILIZAR VAR, UTILIZAR CONST
+
+// + - * / operadores aritmeticos
+const primeiroNumero = 5; // number
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(primeiroNumero + segundoNumero);
