e30169012a1b6273963ce0b5a075834ebe598d34
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-03-06 17:54:44 -0300

    concluido o exercicio somando dois valores no console

--- modulo01_iniciante/aula10/js/script.js
+++ modulo01_iniciante/aula10/js/script.js
@@ -4,3 +4,8 @@
 Receba dois números do úsuario e faça uma conta de adicção e exiba o resultado.
 */
 
+const num1 = Number(prompt('Digite um número: '));
+const num2 = Number(prompt('Digite outro número: '));
+const resultado = num1 + num2
+
+alert(`O resultado foi ${resultado}`)
