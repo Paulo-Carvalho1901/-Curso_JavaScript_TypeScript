e3d3a539aadf3b71a76a315b41ffa76b6000d4d4
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-03-06 12:23:05 -0300

    Operadores artiméticos

---
+++ modulo01_iniciante/aula9/operadores_aritmaticos.js
@@ -0,0 +1,15 @@
+/* 
+Operadores aritméticos
+
++ Adião e concatenação
+- subtração
+/ divisão
+* multiplicação
+** potenciação
+% resto da divisão
+*/
+
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+console.log(num1 + num2 * num3);
