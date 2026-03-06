0585f6237e6b063c6ca249157521872f0c7e908e
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-03-06 18:08:23 -0300

    alterando de const para let

--- modulo01_iniciante/aula10/js/script.js
+++ modulo01_iniciante/aula10/js/script.js
@@ -4,8 +4,8 @@
 Receba dois números do úsuario e faça uma conta de adicção e exiba o resultado.
 */
 
-const num1 = Number(prompt('Digite um número: '));
-const num2 = Number(prompt('Digite outro número: '));
-const resultado = num1 + num2
+let num1 = Number(prompt('Digite um número: '));
+let num2 = Number(prompt('Digite outro número: '));
+let resultado = num1 + num2
 
 alert(`O resultado foi ${resultado}`)
