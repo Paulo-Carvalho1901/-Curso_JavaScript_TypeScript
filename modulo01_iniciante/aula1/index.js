a1bcb21001d523b7a6297e3945815bb7fe0e36d4
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-03-23 01:25:41 -0300

    add templete string

--- modulo01_iniciante/aula1/index.js
+++ modulo01_iniciante/aula1/index.js
@@ -1,6 +1,6 @@
 console.log('Paulo "Carvalho"'); // String = Texto
 console.log(12, 12, 55); // Number = Número
-console.log(`Paulo Roberto`);
+console.log(`Paulo Roberto`); // Templete string
 
 
 console.log(35, 15.85, 'Paulo Carvalho');
